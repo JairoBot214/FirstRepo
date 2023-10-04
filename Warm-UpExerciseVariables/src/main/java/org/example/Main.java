f21191b4f2b1944be5e84cb300c8773bad35574d
jairodaza1 <120524765+jairodaza1@users.noreply.github.com>
2023-10-04 10:01:34 -0500

    VariableWarmUp

--- Warm-UpExerciseVariables/src/main/java/org/example/Main.java
+++ Warm-UpExerciseVariables/src/main/java/org/example/Main.java
@@ -2,7 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("Hello and welcome!");
 
         String personName = "John Doe";
