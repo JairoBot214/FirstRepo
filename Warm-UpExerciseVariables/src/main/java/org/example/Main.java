ba39c86540695c924663c175d46edb42f4820ac3
jairodaza1 <120524765+jairodaza1@users.noreply.github.com>
2023-10-04 09:52:15 -0500

    VariableWarmUp

---
+++ Warm-UpExerciseVariables/src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome!");
+
+        String personName = "John Doe";
+
+        int personAge = 25;
+
+        double productPrice = 19.99;
+
+        float temperatureCelsius = 27.5f;
+
+        boolean isLoggedIn = true;
+
+
+    }
+}
