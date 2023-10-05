ec077745dfffa36a0f5551a9d6538ee0207c433d
jairodaza1 <120524765+jairodaza1@users.noreply.github.com>
2023-10-04 21:16:19 -0500

    Exercise3_BasicCalculator

---
+++ BasicCalculator/src/main/java/org/example/Main.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Enter the first number:");
+        double firstNumber = scanner.nextDouble();
+        System.out.println("Enter the second number:");
+        double secondNumber = scanner.nextDouble();
+
+
+        System.out.println("Do you want to use A, S, M, or D?");
+        scanner.nextLine();
+        String functionUsed = scanner.nextLine();
+
+        if(functionUsed.equalsIgnoreCase("A")){
+            double result = firstNumber + secondNumber;
+            System.out.printf("%.2f + %.2f = %.2f", firstNumber, secondNumber, result);
+        }
+        else if (functionUsed.equalsIgnoreCase("S")){
+            double result = firstNumber - secondNumber;
+            System.out.printf("%.2f - %.2f = %.2f", firstNumber, secondNumber, result);
+        }
+        else if (functionUsed.equalsIgnoreCase("M")){
+            double result = firstNumber * secondNumber;
+            System.out.printf("%.2f * %.2f = %.2f", firstNumber, secondNumber, result);
+        }
+        else if (functionUsed.equalsIgnoreCase("D" )){
+            double result = firstNumber / secondNumber;
+            System.out.printf("%.2f / %.2f = %.2f", firstNumber, secondNumber, result);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
