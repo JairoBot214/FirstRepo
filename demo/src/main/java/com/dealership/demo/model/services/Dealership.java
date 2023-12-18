93e5e14739b39a0dd98d32b3979a8946f9805ef3
jairodaza1 <120524765+jairodaza1@users.noreply.github.com>
2023-12-18 09:40:14 -0600

    new week

---
+++ demo/src/main/java/com/dealership/demo/model/services/Dealership.java
@@ -0,0 +1,59 @@
+package com.dealership.demo.model.services;
+
+import java.util.ArrayList;
+
+public class Dealership {
+    private int dealershipId;
+    private String name;
+    private String address;
+    private String phone;
+    private ArrayList<Vehicle> inventory;
+
+    public Dealership(int dealershipId, String name, String address, String phone) {
+        this.dealershipId = dealershipId;
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = new ArrayList<>();
+    }
+
+    public int getDealershipId() {
+        return dealershipId;
+    }
+
+    public void setDealershipId(int dealershipId) {
+        this.dealershipId = dealershipId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public ArrayList<Vehicle> getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(ArrayList<Vehicle> inventory) {
+        this.inventory = inventory;
+    }
+}
